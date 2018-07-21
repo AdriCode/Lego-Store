b03185b3c8dc0f183bcc1906a6eb007b809d4b9b
Adriana Pérez <csj518@alumni.ku.dk>
2018-07-22 00:57:31 +0200

    ListView showing

--- app/src/main/java/com/example/android/legostore/MainActivity.java
+++ app/src/main/java/com/example/android/legostore/MainActivity.java
@@ -1,31 +1,24 @@
 package com.example.android.legostore;
 
 import android.database.Cursor;
-import android.database.CursorWrapper;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ListView;
-
-import com.example.android.legostore.data.LegoContract;
 import com.example.android.legostore.data.LegoContract.LegoEntry;
 import com.example.android.legostore.data.LegoDBHelper;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase db;
     private Cursor cursor;
-    private ListView listView;
+    private ArrayList<Product> allProducts;
+    private LegoDBHelper mDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +35,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        LegoDBHelper mDBHelper = new LegoDBHelper(this);
-        listView = findViewById(R.id.listView);
+        mDBHelper = new LegoDBHelper(this);
+
+        ListView listView = findViewById(R.id.list_view);
+        allProducts = new ArrayList<>();
         displayData();
+
+        ProductAdapter Adapter = new ProductAdapter(this, allProducts);
+        listView.setAdapter(Adapter);
     }
 
     private void displayData(){
+
+        db = mDBHelper.getReadableDatabase();
+
         //Projection string
         String[] projection = {
                 LegoEntry.COLUMN_ID,
@@ -56,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 LegoEntry.COLUMN_QUANTITY,
         };
 
-        cursor = db.query(LegoEntry.TABLE_NAME, projection, null, null, LegoEntry.COLUMN_QUANTITY, null, LegoEntry.COLUMN_QUANTITY);
-        List<Product> allProducts = new ArrayList<>();
+        cursor = db.query(LegoEntry.TABLE_NAME, projection, null, null, null, null, null);
 
         if (cursor != null){
             try{
@@ -69,12 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 cursor.close();
             }
         }
-
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<Product> arrayAdapter = new ArrayAdapter<Product>(this, android.R.layout.simple_list_item_1, allProducts);
-        listView.setAdapter(arrayAdapter);
     }
 
     private Product getProduct(){
---
+++ app/src/main/java/com/example/android/legostore/ProductAdapter.java
@@ -0,0 +1,60 @@
+package com.example.android.legostore;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends ArrayAdapter<Product> {
+
+    private Product currentProduct;
+
+    public ProductAdapter(@NonNull Context context, ArrayList<Product> products) {
+        super(context, 0, products);
+    }
+
+    /**
+     * Provides a view for an AdapterView
+     *
+     * @param position    The position in the list of data
+     * @param convertView The recycled view to populate.
+     * @param parent      The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the object located at this position in the list
+        currentProduct = getItem(position);
+        ImageView ImageEvent = (ImageView) listItemView.findViewById(R.id.image);
+
+        // Find the TextView in the list_item.xml layout with the Product Name
+        TextView NameView = (TextView) listItemView.findViewById(R.id.product_name);
+        // Get the date from the current event object and set this text on the Product Name TextView
+        NameView.setText(currentProduct.getProductName());
+
+        // Find the TextView in the list_item.xml layout with the Price
+        TextView PriceView = (TextView) listItemView.findViewById(R.id.price);
+        // Get the title from the current event object and set this text on the Price TextView
+        PriceView.setText(currentProduct.getPrice());
+
+        // Find the TextView in the list_item.xml layout with the Quantity
+        TextView QuantityView = (TextView) listItemView.findViewById(R.id.quantity);
+        // Get the title from the current event object and set this text on the Quantity TextView
+        QuantityView.setText(currentProduct.getQuantity());
+
+        return listItemView;
+    }
+}
